a503990882e66b216ba92354ef65eafdfa01158e
abidzan <abidzan@databay.de>
2024-02-02 11:49:49 +0100

    add user list to administration

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +16,24 @@ class AdministrationController extends AbstractController
     {
         return $this->render('administration/index.html.twig', [
             'currentRoute' => $request->attributes->get('_route'),
+        ]);
+    }
+
+    #[Route('/admin/users', name: 'app_admin_users')]
+    public function users(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $usersPerPage = 5;
+        $page = $request->query->get('page', 1);
+
+        $users = $entityManager->getRepository(User::class)->findBy([], ['id' => 'DESC'], $usersPerPage, ($page - 1) * $usersPerPage);
+
+        $userCount = $entityManager->getRepository(User::class)->count([]);
+
+        return $this->render('administration/users.html.twig', [
+            'currentRoute' => $request->attributes->get('_route'),
+            'users' => $users,
+            'maxPage' => ceil($userCount / $usersPerPage),
+            'currentPage' => $page,
         ]);
     }
 }
