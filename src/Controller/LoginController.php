90d304d2bd68b16fab185d8e76ec18a41e999270
Aaron Bidzan <bidzanaaron@gmail.com>
2024-02-08 22:44:09 +0100

    make basis for email verification

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class LoginController extends AbstractController
 {
@@ -29,6 +34,33 @@ class LoginController extends AbstractController
     {
         $security->logout();
 
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/verify', name: 'app_verify_email')]
+    public function verifyEmail(Request $request, VerifyEmailHelperInterface $verifyEmailHelper, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user = $userRepository->find($request->query->get('id'));
+        if (!$user) {
+            throw $this->createNotFoundException();
+        }
+
+        try {
+            $verifyEmailHelper->validateEmailConfirmation(
+                $request->getUri(),
+                $user->getId(),
+                $user->getEmail(),
+            );
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('verify_email_error', $e->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        $user->setEmailVerified(true);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Account Verified! You can now log in.');
         return $this->redirectToRoute('app_login');
     }
 }
