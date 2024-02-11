0233e43698bbf73fe0db2dfe17659a6df4d9264d
abidzan <abidzan@databay.de>
2024-02-11 09:46:15 +0100

    add changelogs

---
+++ src/Controller/ChangelogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use JsonException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ChangelogController extends AbstractController
+{
+    #[Route('/changelog', name: 'app_changelog')]
+    public function index(Request $request): Response
+    {
+        $changelog = file_get_contents(__DIR__ . '/../../public/info/changelog.json');
+        try {
+            $changelog = json_decode($changelog, true, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            $changelog = [];
+        }
+
+        return $this->render('changelog/index.html.twig', [
+            'changelog' => $changelog,
+            'currentRoute' => $request->attributes->get('_route'),
+        ]);
+    }
+}
